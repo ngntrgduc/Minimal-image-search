13b3db1d597dcf086844d0c4b8d665946d6aa1cf
ngntrgduc <trungducnguyen1511@gmail.com>
2024-07-31 16:07:24 +0700

    Remove context menus, add validation for url type

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    browser.tabs.update({
-        url: "https://lens.google.com/uploadbyurl?url=" + tabs[0].url
-    });
-    window.close();
+    let url = tabs[0].url;
+    const urlType = url.split('.').pop();
+    const imageTypes = ["jpg", "png", "jpeg"] 
+    if (imageTypes.includes(urlType)) {
+        browser.tabs.update({
+            url: "https://lens.google.com/uploadbyurl?url=" + url
+        });
+        window.close();
+    }
 });
